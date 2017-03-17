d15c76078f007571f5b7c97e46390caac4b64c92
😊Nitin Gurram😊 <nigurr@microsoft.com>
2017-03-17 15:45:56 +0530

    First Commit

---
+++ UnitTestProject1/NUnit.Tests1/TestClass.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+namespace NUnit.Tests1
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Assert.Pass("Your first passing test");
+        }
+
+        [Test, Author("NunitTest"), Category("Nunit")]
+        public void TestMethod2()
+        {
+            Assert.Pass("Your first passing test");
+        }
+
+        [Test, Ignore("No reason"), Author("NunitTest")]
+        public void TestMethod3()
+        {
+            Assert.Pass("Your first passing test");
+        }
+
+        [Test, Author("NunitTest"), Platform("x86")]
+        public void TestMethod4()
+        {
+            Assert.Pass("Your first passing test");
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod, Priority(0), Owner("test")]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true, "It shouldn't fail");
+        }
+
+        [TestMethod, Priority(0), Owner("test")]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(false, "It should fail");
+        }
+
+        [TestMethod, Priority(0), Owner("test")]
+        [Ignore]
+        public void TestMethod3()
+        {
+            Assert.IsTrue(false, "This shouldn't be executed");
+        }
+
+        [TestMethod, Priority(3), Owner("test")]
+        public void TestMethod4()
+        {
+            Assert.IsTrue(true, "Testing with Priority");
+        }
+
+        [TestMethod, Priority(2), Owner("test"), TestCategory("TestCat")]
+        public void TestMethod5()
+        {
+            Assert.IsTrue(true, "Testing with some category");
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.IsTrue(true, "Another simple test which throws exception");
+            throw new Exception("Some random exception");
+        }
+    }
+}
