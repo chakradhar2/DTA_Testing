b0f0fafd8e144e17edc560bb889f1aa31df64fe9
Kavipriya Adhinarayanan <kaadhina@microsoft.com>
2017-04-03 18:38:43 +0530

    Adding project for TIA

---
+++ UnitTestProject1/TIAProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TIAProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.Fail();
+        }
+    }
+}
