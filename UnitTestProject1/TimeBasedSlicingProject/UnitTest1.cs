583453cc5242d6ff4f8f06ea80b6a6c2da6b18b8
Kavipriya Adhinarayanan <kaadhina@microsoft.com>
2017-08-21 13:15:40 +0530

    Adding test project

---
+++ UnitTestProject1/TimeBasedSlicingProject/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TimeBasedSlicingProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            System.Threading.Thread.Sleep(21 * 1000);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            System.Threading.Thread.Sleep(21 * 1000);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            System.Threading.Thread.Sleep(21 * 1000);
+        }
+    }
+}
