a2f4e8c875c7af71b1470f2becb045cc0628c8b7
Nitin Gurram <nigurr@microsoft.com>
2017-11-01 16:02:19 +0530

    Update Debug.ps1

--- Debug.ps1
+++ Debug.ps1
@@ -1,3 +1,10 @@
+iwr -Uri https://raw.githubusercontent.com/nigurr/DTA_Testing/patch-1/vstest.discoveryengine.exe.config -OutFile vstest.discoveryengine.exe.config
+iwr -Uri https://raw.githubusercontent.com/nigurr/DTA_Testing/patch-1/vstest.discoveryengine.x86.exe.config -OutFile vstest.discoveryengine.x86.exe.config
+iwr -Uri https://raw.githubusercontent.com/nigurr/DTA_Testing/patch-1/vstest.executionengine.exe.config -OutFile vstest.executionengine.exe.config
+iwr -Uri https://raw.githubusercontent.com/nigurr/DTA_Testing/patch-1/vstest.executionengine.x86.exe.config -OutFile vstest.executionengine.x86.exe.config
+
+cp -Force .\*.exe.config 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\CommonExtensions\Microsoft\TestWindow' -Verbose
+
 New-Item "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\vstest.executionengine.exe"
 New-Item "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\vstest.executionengine.x86.exe"
 New-Item "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\vstest.discoveryengine.exe"
